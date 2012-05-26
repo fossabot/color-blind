b098bfc0f9fcc1456a66ae7f5456fc02dd9c2c1b
Mr Axilus <esmith@projectaxil.us>
2012-05-27 00:55:17 +0100

    Got a basic box2d demo working.

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -6,6 +6,7 @@ define(function(require) {
   require('jquery');
   require('box2d/javascript/Box2D/box2d');
 
+
   stats = new Stats();
   stats.setMode(0); // 0: fps, 1: ms
 
@@ -22,21 +23,8 @@ define(function(require) {
   function init() {
       var bodyDef, debugDraw, fixDef, SCALE;
 
-      /*
-      var b2Vec2         = Box2D.Common.Math.b2Vec2,
-          b2BodyDef      = Box2D.Dynamics.b2BodyDef,
-          b2Body         = Box2D.Dynamics.b2Body,
-          b2FixtureDef   = Box2D.Dynamics.b2FixtureDef,
-          b2Fixture      = Box2D.Dynamics.b2Fixture,
-          b2World        = Box2D.Dynamics.b2World,
-          b2MassData     = Box2D.Collision.b2MassData,
-          b2PolygonShape = Box2D.Collision.b2PolygonShape,
-          b2CircleShape  = Box2D.Collision.b2CircleShape,
-          b2DebugDraw    = Box2D.Dynamics.b2DebugDraw;
-    */
-
       world = new b2World(
-              new b2Vec2(0, 10), // gravity
+              new b2Vec2(0, -10), // gravity
               true // allow sleep
      );
 
@@ -46,32 +34,32 @@ define(function(require) {
 
      fixDef.density     = 1.0;
      fixDef.friction    = 0.5;
-     fixDef.restitution = 0.2;
+     fixDef.restitution = 0.75;
 
-     bodyDef = b2BodyDef();
+     bodyDef = new b2BodyDef();
 
      // create ground
      bodyDef.type = b2Body.b2_staticBody;
 
      // positions the center of the object (not upper left!)
      bodyDef.position.x = canvas.width / 2 / SCALE;
-     bodyDef.position.y = canvas.height / SCALE;
+     bodyDef.position.y = 7 / SCALE;
 
      fixDef.shape = new b2PolygonShape();
 
      // half width, half height. eg actual height here is 1 unit
-     fixDef.shape.setAsBox((600 / SCALE) / 2, (10 / SCALE) / 2);
+     fixDef.shape.SetAsBox((600 / SCALE) / 2, (10 / SCALE) / 2);
      world.CreateBody(bodyDef).CreateFixture(fixDef);
 
      // create some objects
      bodyDef.type = b2Body.b2_dynamicBody;
 
-     for (var i = 0; i < 150; i++) {
+     for (var i = 0; i < 20; i++) {
          if (Math.random() > 0.5) {
              fixDef.shape = new b2PolygonShape();
              fixDef.shape.SetAsBox(
                      Math.random() + 0.1, // half width
-                     Maht.random() + 0.1 // half height
+                     Math.random() + 0.1 // half height
             );
         } else {
             fixDef.shape = new b2CircleShape(
@@ -79,8 +67,8 @@ define(function(require) {
             );
         }
 
-        bodyDef.position.x = Math.random() * 25;
-        bodyDef.position.y = Math.random() * 10;
+        bodyDef.position.x = Math.random() * 20;
+        bodyDef.position.y = Math.random() * 20;
         world.CreateBody(bodyDef).CreateFixture(fixDef);
     }
 
@@ -92,7 +80,7 @@ define(function(require) {
     debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
     world.SetDebugDraw(debugDraw);
 
-    setTimeout(init, 6000);
+    setTimeout(init, 10000);
   }
 
 
