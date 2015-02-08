8fbc9e495f45b4a487cdfad3e3f31d194b0934a0
mr axilus <esmith@projectaxil.us>
2015-02-08 23:21:24 +0000

    Refactor with config file for easy level changes

--- game.py
+++ game.py
@@ -1,98 +1,73 @@
-from math import pi, sin, cos
 from pyglet.gl import *
 from pyglet.window import key
 import pyglet 
 import pymunk
+import yaml
 
-window = pyglet.window.Window(width=1280, height=720)
-fps = 60.0
+config = yaml.load(file('config.yaml', 'r'))
+window = pyglet.window.Window(width=config['window']['width'],
+                              height=config['window']['height'])
 space = pymunk.Space()
-space.gravity = (0.0, -200.0)
-ball, floor = None, None
+space.gravity = (config['gravity']['x'], config['gravity']['y'])
+shapes = []
+
+
+def main():
+  setup()
+  pyglet.clock.schedule_interval(update, 1 / config['fps'])
+  pyglet.app.run()
+
+
+def setup():
+  global config
+  global shapes
+  for shape in config['shapes']:
+      shapes.append(create_shape(tuple(shape['position']),
+                                 map(tuple, shape['vertices']), 
+                                 shape['elasticity'],
+                                 shape['friction']))
 
 
 def update(dt):
   global space
-  global keys
-  global ball
-  ball.body.angular_velocity *= 0.95
   space.step(dt)
 
 
-@window.event
-def on_key_press(symbol, modifiers):
-  global ball
-  if symbol == key.LEFT:
-    ball.body.apply_impulse((-50, 0))
-  if symbol == key.RIGHT:
-    ball.body.apply_impulse((50, 0))
-  if symbol == key.UP:
-    ball.body.apply_impulse((0, 200))
-
-
-@window.event
-def on_draw():
-  global ball
-  global floor
-  glClear(GL_COLOR_BUFFER_BIT)
-  glColor3f(1, 1, 1)
-  window.clear()
-  if ball is None:
-    ball = add_ball(space)
-    floor = add_floor(space)
-  draw_ball(ball)
-  draw_line((0, 15), (window.width, 15))
-  
-
-
-
-def add_ball(space):
-  mass = 1
-  radius = 14
-  inertia = pymunk.moment_for_circle(mass, 0, radius)
-  body = pymunk.Body(mass, inertia)
-  body.position = window.width // 2, window.height // 2
-  shape = pymunk.Circle(body, radius)
-  shape.friction = 0.1
-  shape.elasticity = 0.5
-  space.add(body, shape)
-  return shape
-
-
-def add_floor(space):
+def create_shape(position, vertices, elasticity, friction):
+    global space
     body = pymunk.Body()
-    body.position = window.width // 2, 0
-    shape = pymunk.Segment(body, (-window.width // 2, 0), (window.width // 2, 0), 15)
-    shape.elasticity = 0.5
-    shape.friction = 0.5
+    body.position = position
+    shape = pymunk.Poly(body, vertices)
+    shape.elasticity = elasticity
+    shape.friction = friction
     space.add(shape)
     return shape
 
 
-def draw_ball(ball):
-  draw_circle(ball.body.position.x, ball.body.position.y, ball.radius)
-
-
-def draw_circle(x, y, radius):
-  iterations = int(2 * radius * pi)
-  s = sin((2 * pi) / iterations)
-  c = cos((2 * pi) / iterations)
-  dx, dy = radius, 0
-
-  glBegin(GL_TRIANGLE_FAN)
-  glVertex2f(x, y)
-  for i in range((iterations + 1) - 2):
-    glVertex2f(x + dx, y + dy)
-    dx, dy = ((dx * c) - (dy * s)), ((dy * c) + (dx * s))
+def draw_rectangle(vertices):
+  glBegin(GL_LINE_LOOP)
+  for vertex in vertices:
+      glVertex2f(vertex[0], vertex[1])
   glEnd()
 
-def draw_line(a, b):
-  glBegin(GL_LINES)
-  glVertex2f(a[0], a[1])
-  glVertex2f(b[0], b[1])
-  glEnd()
+
+@window.event
+def on_key_press(symbol, modifiers):
+  global objects
+  if symbol == key.LEFT:
+    ball.body.apply_impulse((-50, 0))
+
+
+@window.event
+def on_draw():
+  global shapes
+  glClear(GL_COLOR_BUFFER_BIT)
+  glColor3f(0.9, 0.9, 0.9)
+  window.clear()
+  for shape in shapes:
+    draw_rectangle(shape.verts)
 
 
 if __name__ == '__main__':
-  pyglet.clock.schedule_interval(update, 1 / fps)
-  pyglet.app.run()
+  main()
+
